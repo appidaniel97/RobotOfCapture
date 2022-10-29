39ea7cf9579ca6c67a23652475955b3e6a0b81ae
appidaniel97 <appidaniel97@gmail.com>
2022-10-29 18:02:38 -0300

    Armazened datas

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+//Commands for capture date in sites
+//document.querySelector('.ratingDate').innerHTML - Date of reviewed
+//document.querySelector('.noQuotes').innerHTML - Title about review
+//document.querySelector('.partial_entry').innerHTML - Comment about review
+
 console.log("Bem vindo ao Roboto!");
 
 const puppeteer = require('puppeteer');
 
 
-(async () => {
+async function robot(){
     const browser = await puppeteer.launch({ headless: false});
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+    const vezpaTijucaUrl = `https://www.tripadvisor.com/Restaurant_Review-g303506-d9969088-Reviews-Vezpa_Pizzas_Tijuca-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html`
+    await page.goto(vezpaTijucaUrl);
     await page.screenshot({path: 'example.png'});
-
     //await browser.close();
-})();
+
+    const dateReviewd = await page.evaluate(() => {
+        return document.querySelector('.ratingDate').innerHTML
+    })
+    const titleReviewd = await page.evaluate(() => {
+        return document.querySelector('.noQuotes').innerHTML
+    })
+
+    console.log(`Data da avaliação:${dateReviewd}`)
+    console.log(`Titulo da avaliação:${titleReviewd}`)
+}
+
+robot();
     
